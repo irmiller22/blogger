98463ea6969e3a2d7ed7bfe33f020dbc505c5106
Ian Miller <irmiller22@gmail.com>
2015-04-08 11:42:49 -0400

    got additional parameter working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root "posts#index"
+  root to: "posts#index"
 
   resources :posts do
     resources :comments, only: [:create, :show, :destroy]
