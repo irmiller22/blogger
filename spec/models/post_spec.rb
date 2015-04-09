1aa3d7750638ca27c714b9ecc5c6932402bea099
Ian Miller <irmiller22@gmail.com>
2015-04-09 15:37:52 -0400

    finished rest of factory girl tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Post, type: :model do
+  it 'has a valid factory' do
+    expect(build(:post)).to be_valid
+  end
+
+  describe 'model validations' do
+    it 'is valid with a name and content' do
+      post = build(:post)
+      expect(post).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      post = build(:post, name: nil)
+      expect(post).to_not be_valid
+    end
+
+    it 'is invalid without content' do
+      post = build(:post, content: nil)
+      expect(post).to_not be_valid
+    end
+  end
+
+  describe 'model attributes' do
+    let(:post) { create(:post) }
+
+    it 'has a #name attribute' do
+      expect(post.name).to eq("Delivering Goods")
+    end
+
+    it 'has a #content attribute' do
+      expect(post.content).to eq("In order to deliver goods, you should...")
+    end
+  end
+
 end
